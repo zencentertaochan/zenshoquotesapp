bafcea57e9ee4af59fce0562c534cf266e604a6e
Axel Jung <ajung@ajung.de>
2017-02-26 00:51:12 +0100

    templates

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,57 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "cordova-plugin-x-socialsharing.SocialSharing",
+        "file": "plugins/cordova-plugin-x-socialsharing/www/SocialSharing.js",
+        "pluginId": "cordova-plugin-x-socialsharing",
+        "clobbers": [
+            "window.plugins.socialsharing"
+        ]
+    },
+    {
+        "id": "cordova-plugin-admobpro.AdMob",
+        "file": "plugins/cordova-plugin-admobpro/www/AdMob.js",
+        "pluginId": "cordova-plugin-admobpro",
+        "clobbers": [
+            "window.AdMob"
+        ]
+    },
+    {
+        "id": "com.phonegap.plugins.PushPlugin.PushNotification",
+        "file": "plugins/com.phonegap.plugins.PushPlugin/www/PushNotification.js",
+        "pluginId": "com.phonegap.plugins.PushPlugin",
+        "clobbers": [
+            "PushNotification"
+        ]
+    },
+    {
+        "id": "cordova-plugin-device.device",
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "pluginId": "cordova-plugin-device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "id": "cordova-plugin-email-composer.EmailComposer",
+        "file": "plugins/cordova-plugin-email-composer/www/email_composer.js",
+        "pluginId": "cordova-plugin-email-composer",
+        "clobbers": [
+            "cordova.plugins.email",
+            "plugin.email"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.3.1",
+    "cordova-plugin-x-socialsharing": "5.1.6",
+    "cordova-plugin-extension": "1.5.1",
+    "cordova-plugin-admobpro": "2.26.0",
+    "com.phonegap.plugins.PushPlugin": "2.5.0",
+    "cordova-plugin-device": "1.1.4",
+    "cordova-plugin-email-composer": "0.8.3"
+};
+// BOTTOM OF METADATA
+});
---
+++ www/js/common/app.js
@@ -0,0 +1,24 @@
+app.constant("Config", {
+  "ApiUrl": "data/feed.json",
+  "PhotoUrl": "data/photos.json",
+  "CommentUrl": "data/comments.json",
+  "FriendsUrl": "data/friends.json",
+  "MessagesUrl": "data/messages.json",
+  "MessageUrl": "data/message.json",
+  "ProductUrl": "data/products.json",
+  "WordPress": "http://data.dearpins.com/wp/",
+})
+// config contact
+app.constant("ConfigContact", {
+  "EmailId": "weblogtemplatesnet@gmail.com",
+  "ContactSubject": "Contact"
+})
+// config admon
+app.constant("ConfigAdmob", {
+  "interstitial": "ca-app-pub-3940256099942544/1033173712",
+  "banner": "ca-app-pub-3940256099942544/6300978111"
+})
+// push notification
+app.constant("PushNoti", {
+  "senderID": "321948427526",
+})
